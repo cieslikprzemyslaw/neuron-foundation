17f9b33a59b5d5008354e8dda16df8932ee97e71
Winnicki Antoni (STUD) <01133502@pw.edu.pl>
2021-03-01 20:33:21 +0100

    [Add] gallery site

---
+++ src/pages/gallery.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Nav from '../components/staticComponents/Nav';
+import MobileNav from '../components/staticComponents/MobileNav';
+import styled from 'styled-components';
+
+const Gallery = () => {
+    return (
+        <div>
+            <Nav />
+            <MobileNav />
+        </div>
+    );
+};
+
+export default Gallery;
