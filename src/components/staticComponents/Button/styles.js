92b67d25f4c696e7d690c68a606c14a5e8d46f3f
Przemyslaw Cieslik <cprzemek91@hotmail.com>
2021-03-12 22:17:14 +0000

    Create Button

---
+++ src/components/staticComponents/Button/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 18px 24px 17px;
+    border: 2px solid #0e0e0e;
+    border-radius: 10px;
+    color: #fff;
+    font-weight: 600;
+    background-color: #0e0e0e;
+    text-transform: uppercase;
+    letter-spacing: 0.2rem;
+    box-shadow: 6px 16px 20px 0px rgba(0, 0, 0, 0.25);
+    cursor: pointer;
+    height: 50px;
+`;
+
+export { Button };
