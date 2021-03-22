a6dbe304ec34968e0daefce2a7432fc9b17d4eda
Przemyslaw Cieslik <cprzemek91@hotmail.com>
2021-03-22 23:20:23 +0000

    Add Validation to Form

--- src/components/neuronFoundationHome/NewsSubscription/index.js
+++ src/components/neuronFoundationHome/NewsSubscription/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Button from '../../staticComponents/Button';
 import { Newsletter, NewsletterSubSection, NewsletterText, NewsletterInput } from './styles';
 import { useIntl } from 'gatsby-plugin-intl';
@@ -9,6 +9,25 @@ const index = () => {
         `${intl.formatMessage({ id: `newslatter.email` })}`,
     );
 
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const clearInput = () => {
+        setInputValue('');
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const regex = /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@[*[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+]*/;
+        if (regex.test(inputValue)) {
+            return console.error('dziala');
+        }
+        if (!regex.test(inputValue)) {
+            return console.error('nie działa');
+        }
+    };
+
     return (
         <Newsletter>
             <NewsletterSubSection>
@@ -16,8 +35,20 @@ const index = () => {
                 <NewsletterText>{intl.formatMessage({ id: `newslatter.title2` })}</NewsletterText>
             </NewsletterSubSection>
             <NewsletterSubSection>
-                <NewsletterInput type="text" value={inputValue} />
-                <Button onClick={() => console.log(`working`)} text={`ZAPISZ SIĘ`} width={212} />
+                <NewsletterInput
+                    type="text"
+                    type="email"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onClick={clearInput}
+                />
+                <Button
+                    type="submit"
+                    form="form1"
+                    onClick={handleSubmit}
+                    text={`ZAPISZ SIĘ`}
+                    width={212}
+                />
             </NewsletterSubSection>
         </Newsletter>
     );
